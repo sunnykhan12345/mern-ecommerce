85e532fcceeb92928cb889631cdb539b38a972a9
sana ullah <sunnykhansadda@gmail.com>
2026-07-06 17:17:09 +0500

    complete products create  delete update and get all api

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,6 @@
 import Product from "../models/Product.js";
 
+// create a new product ['/api/products', POST]
 export const createProduct = async (req, res) => {
   try {
     const { title, description, price, image, category, stock } = req.body;
@@ -13,6 +14,40 @@ export const createProduct = async (req, res) => {
     });
     await product.save();
     res.status(201).json({ message: "Product created successfully", product });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+// get all products ['/api/products', GET]
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find().sort({ createdAt: -1 });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+// update a product ['/api/products/:id', PUT]
+export const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description, price, image, category, stock } = req.body;
+    const product = await Product.findByIdAndUpdate(
+      id,
+      { title, description, price, image, category, stock },
+      { new: true },
+    );
+    res.status(200).json({ message: "Product updated successfully", product });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+// delete a product ['/api/products/:id', DELETE]
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    res.status(200).json({ message: "Product deleted successfully", product });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { createProduct } from "../controllers/productController.js";
+import {
+  createProduct,
+  getProducts,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/productController.js";
 const router = express.Router();
-
-router.post("/", createProduct);
-
+router.post("/add", createProduct);
+router.get("/", getProducts);
+router.put("/update/:id", updateProduct);
+router.delete("/delete/:id", deleteProduct);
 export default router;
